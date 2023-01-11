aaee1e3273a5c5b1e38843e4d88672255b9eea7f
Ideassion <Ideassion@EDITH>
2023-01-11 17:06:34 +0530

    First try

---
+++ Ideassion/src/Reverse_Char.java
@@ -0,0 +1,15 @@
+public class Reverse_Char {
+
+	public static void main(String[] args) {
+		String str="String";
+		char[] ar= str.toCharArray();
+		for(int i=ar.length-1;i>=0;i--)
+		{
+			System.out.print(ar[i]+" ");
+			
+		}
+		
+	
+	}
+}
+ 
