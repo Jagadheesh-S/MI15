9bf4897fc7f0591841366dfd8fe93d35a84f26cc
Ideassion <Ideassion@EDITH>
2023-01-18 15:01:33 +0530

    Class and object - 18/1/23

--- Ideassion/src/cl_ob.java
+++ Ideassion/src/cl_ob.java
@@ -1,22 +1,26 @@
 //Class and object program....
-
+class Student
+{
+	String name;
+	 int rollno,age;		 
+void info()
+{
+	  System.out.println("Name: "+name);
+	  System.out.println("Roll Number: "+rollno);
+	  System.out.println("Age: "+age);
+}
 public class cl_ob {   
-		 String name;
-		 int rollno,age;		 
-void info(){
-		  System.out.println("Name: "+name);
-		  System.out.println("Roll Number: "+rollno);
-		  System.out.println("Age: "+age);
-}  
 public static void main(String[] args) {
-			cl_ob student = new cl_ob();
+			Student student = new Student();
 			student.name = "Ramesh";
 			student.rollno = 253;
 			student.age = 25;	
 			student.info();
+}
 			
-	}
-}  
+}
+}
+ 
 		
 
 	
