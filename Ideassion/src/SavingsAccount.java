728b85ce8abffb08112eafeab7237b6f8991cc03
Jagadheesh-S <122447556+Jagadheesh-S@users.noreply.github.com>
2023-01-25 16:00:29 +0530

    Add files via upload

---
+++ Ideassion/src/SavingsAccount.java
@@ -0,0 +1,27 @@
+package Banking;
+
+	public class SavingsAccount extends BankAccount {
+		public SavingsAccount(String accNumber, String accName) {
+			super(accNumber, accName);
+			
+		}
+
+		public boolean deposit(double amount) {
+			if (super.deposit(amount)) {
+				
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean withdraw(double amount) {
+			if (super.withdraw(amount)) {
+				
+				return true;
+			}
+			return false;
+		}
+
+	}
+
+
